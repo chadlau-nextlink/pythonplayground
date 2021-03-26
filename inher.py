a348e7484f2ec1b47e40ee9ed4973a5ba5e923fa
chadlau-nextlink <chadlau.nextlink@gmail.com>
2021-03-26 11:17:10 +0800

    test

---
+++ inher.py
@@ -0,0 +1,15 @@
+class Chef:
+
+    def make_chicken(self):
+        print("Chicken.")
+        
+    def make_salad(self):
+        print("Salad.")
+
+class ChineseChef(Chef):
+
+    def make_chicken(self):
+        print("Chicken.")
+        
+    def make_salad(self):
+        print("Salad.")
